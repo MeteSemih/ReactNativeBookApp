935409a949c7b2e70f21b16655169e046faec2f4
Mete <korganmete@hotmail.com>
2025-06-20 02:55:07 +0300

    Update all page uı

--- src/component/CardComponent.js
+++ src/component/CardComponent.js
@@ -1,50 +1,52 @@
-import { useNavigation } from '@react-navigation/native'
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-const CardComponent = ({id,title,image,villains,pages,publisher}) => {
-    const navigation = useNavigation()
+const CardComponent = ({ id, title, image, villains, pages, publisher }) => {
+  const navigation = useNavigation();
 
-   const gotoDetail = () => {
-    navigation.navigate('Detail',{
-        id,
-        title,
-        publisher,
-        pages,
-        villains,
-        image
-    })
-   }
- 
+  const gotoDetail = () => {
+    navigation.navigate('Detail', {
+      id,
+      title,
+      publisher,
+      pages,
+      villains,
+      image,
+    });
+  };
+
   return (
-    <TouchableOpacity onPress={gotoDetail} style={styles.container}>
-      <Text style={styles.title}>{title}</Text>
+    <TouchableOpacity onPress={gotoDetail} style={styles.card}>
       <Image source={{ uri: image }} style={styles.image} />
+      <View style={styles.textContainer}>
+        <Text style={styles.title} numberOfLines={2} ellipsizeMode="tail">
+          {title}
+        </Text>
+      </View>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
-export default CardComponent
+export default CardComponent;
 
 const styles = StyleSheet.create({
-  container: {
-    width: 160,
+  card: {
+    flex: 1,
     margin: 8,
     backgroundColor: '#fff',
     borderRadius: 12,
     overflow: 'hidden',
-    elevation: 3,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 4,
+    elevation: 4,
+    maxWidth: '48%',
   },
   image: {
-    width: 120,
-    height: 120,
-    alignSelf:'center',
-    borderTopLeftRadius: 12,
-    borderTopRightRadius: 12,
-    resizeMode: 'stretch'
+    width: '100%',
+    height: 200,
+    resizeMode: 'cover',
   },
   textContainer: {
     padding: 12,
@@ -54,6 +56,5 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#333',
     textAlign: 'center',
-    paddingBottom:8,
   },
 });
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,36 +1,35 @@
-import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchBooks } from '../store/slice/bookSlice'
-import CardComponent from '../component/CardComponent'
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchBooks } from '../store/slice/bookSlice';
+import CardComponent from '../component/CardComponent';
 
 const Home = () => {
+  const { list, error, loading } = useSelector((state) => state.books);
+  const dispatch = useDispatch();
 
-    const {list,error,loading} = useSelector((state)=> state.books)
-    const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
 
-    useEffect(() => {
-     dispatch(fetchBooks())
-    }, [dispatch])
-
-    if(loading) {
-     return(
-       <View style={styles.center}>
-        <ActivityIndicator size="large" color="#0000ff" />
+  if (loading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size="large" color="#FF6F3C" />
         <Text>Loading...</Text>
-       </View>
-       )
-    }
-   
-      if (error) {
+      </View>
+    );
+  }
+
+  if (error) {
     return (
       <View style={styles.center}>
         <Text style={{ color: 'red' }}>Error: {error}</Text>
       </View>
     );
   }
-    
-    const renderItem = ({ item }) => (
+
+  const renderItem = ({ item }) => (
     <CardComponent
       title={item.Title}
       image={`https://covers.openlibrary.org/b/isbn/${item.ISBN}-L.jpg`}
@@ -43,45 +42,42 @@ const Home = () => {
 
   return (
     <View style={styles.container}>
-        <Text style={styles.head}>Stephan King Books</Text>
+      <Text style={styles.head}>Stephen King Books</Text>
       <FlatList
         data={list}
-        numColumns={3}
+        numColumns={2}
         keyExtractor={(item) => item.id.toString()}
         renderItem={renderItem}
+        contentContainerStyle={styles.listContent}
+        showsVerticalScrollIndicator={false}
       />
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fdfdfd',
+    paddingHorizontal: 16,
+    paddingTop: 24,
+  },
   center: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-  item: {
-    backgroundColor: '#eee',
-    padding: 16,
-    marginBottom: 10,
-    borderRadius: 8,
-  },
-  title: {
+  head: {
+    textAlign: 'center',
+    fontSize: 28,
     fontWeight: 'bold',
-    fontSize: 16,
+    marginBottom: 16,
+    color: '#FF6F3C',
+    paddingTop:20,
   },
-  head:{
-    textAlign:"center",
-    fontSize:24,
-    alignItems:"center",
-    marginTop:20,
-    color:'#FF6F3C',
-  }
-  
+  listContent: {
+    paddingBottom: 20,
+  },
 });
